6c10c3ba270322f03678aa0d33b3d8084438a763
justin <jfc4050@gmail.com>
2019-03-10 18:14:42 -0700

    AnchorEncoder and RegionEncoder represented as separate classes

--- data/encoding.py
+++ data/encoding.py
@@ -9,16 +9,17 @@ from ml_utils.bbox_utils import ijhw_to_ijij, compute_ious
 from . import ObjectLabel
 
 
-class LabelEncoder(abc.ABC):
-    """handles encoding of ground-truth labels."""
+class FRCNNLabelEncoder(abc.ABC):
+    """handles encoding of ground-truth labels according to
+    https://arxiv.org/abs/1506.01497"""
     @abc.abstractmethod
     def __call__(self, labels):
         raise NotImplementedError
 
 
-class FRCNNEncoder(LabelEncoder):
-    """anchorwise class and bounding-box offset encoding
-    according to https://arxiv.org/abs/1506.01497
+class AnchorEncoder(FRCNNLabelEncoder):
+    """assign target classes and bounding box offsets anchorwise based on
+    iou with closest ground-truth bounding box.
 
     Args:
         anchors: (|A|, 4); bounding box priors. must be ijhw and fractional.
@@ -33,13 +34,13 @@ class FRCNNEncoder(LabelEncoder):
             iou_thresh: float = 0.5,
             iou_margin: float = 0.2
     ):
-        self._anchors = anchors
+        self.anchors = anchors
         self._iou_thresh = iou_thresh
         self._iou_margin = iou_margin
 
         ### determine which anchors cross image boundaries. These are ignored
         ### during training.
-        anchors_ijij = ijhw_to_ijij(self._anchors)
+        anchors_ijij = ijhw_to_ijij(self.anchors)
         self._crosses_boundary = np.logical_or(
             np.any(anchors_ijij <= 0, axis=1),
             np.any(anchors_ijij >= 1, axis=1)
@@ -58,7 +59,7 @@ class FRCNNEncoder(LabelEncoder):
                 for some ground-truth box b
 
         Args:
-            labels: sequence of object labels.
+            labels: ground-truth object label for each object in image.
 
         Returns:
             loss_weights: (|A|,) anchorwise weight assigned to losses for
@@ -66,12 +67,11 @@ class FRCNNEncoder(LabelEncoder):
             c_star: (|A|,) anchorwise assigned class values.
             b_star: (|A|, 4) anchorwise offset from assigned bounding box.
         """
-        ### start with label conversion
-        # classes: (|O|,), bboxes: (|O|, 4)
-        classes = np.array([label.class_id for label in labels])
-        bboxes = np.array([label.box for label in labels])
+        ### unzip labels
+        boxes = np.array([label.box for label in labels])  # (|O|, 4)
 
-        ious = compute_ious(self._anchors, bboxes)  # (|A|, |B|)
+        ### assign ground-truth boxes anchorwise
+        ious = compute_ious(self.anchors, boxes)  # (|A|, |B|)
         anchwise_best_gt_ind = ious.argmax(1)  # (|A|,)
         anchwise_best_iou = ious.max(1)  # (|A|,)
 
@@ -82,23 +82,70 @@ class FRCNNEncoder(LabelEncoder):
         )  # (|A|,)
 
         ### c_star encoding
-        c_star = classes[anchwise_best_gt_ind]  # (|A|,)
-        is_best_anchor = np.zeros(len(self._anchors))  # (|A|,)
+        is_best_anchor = np.zeros(len(self.anchors))  # (|A|,)
         is_best_anchor[ious.argmax(0)] = 1
-        neg_mask = np.logical_and(
-            anchwise_best_iou < self._iou_thresh, np.logical_not(is_best_anchor)
-        )  # (|A|,)
-        c_star[neg_mask] = 0
+        c_star = np.logical_or(
+            anchwise_best_iou > self._iou_thresh, is_best_anchor
+        )
 
         ### b_star encoding
         b_star = frcnn_box_encode(
-            bboxes[anchwise_best_gt_ind, :], self._anchors
+            self.anchors, boxes[anchwise_best_gt_ind, :]
         )  # (|A|, 4)
 
         return loss_weights, c_star, b_star
 
 
-def frcnn_box_encode(boxes: np.ndarray, anchors: np.ndarray) -> np.ndarray:
+class RegionEncoder(FRCNNLabelEncoder):
+    """assign target classes and bounding box offsets regionwise based on
+    iou with closest ground-truth bounding box.
+
+    Args:
+        iou_thresh: if iou(region, gt-bbox) > iou_thresh for any gt-bbox,
+            region is assigned a class.
+    """
+    def __init__(self, iou_thresh: float) -> None:
+        self._iou_thresh = iou_thresh
+
+    def __call__(
+            self,
+            regions: np.ndarray,
+            labels: Sequence[ObjectLabel]
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """encode human-readable ground-truth frame instances into
+        network-readable frame instances, according to Faster RCNN conventions.
+
+        Args:
+            regions: (|R|, 4) predicted region proposal boxes.
+            labels: ground-truth object label for each object in image.
+
+        Returns:
+            c_star: (|R|,) regionwise class assignments.
+            b_star: (|R|, 4) regionwise bounding box offsets from assigned
+                ground-truth box.
+        """
+        ### unzip labels
+        classes = np.array([label.class_id for label in labels])
+        boxes = np.array([label.bbox for label in labels])
+
+        ### assign ground-truth boxes regionwise
+        ious = compute_ious(regions, boxes)  # (|A|, |B|)
+        regionwise_best_gt = ious.argmax(1)  # (|A|,)
+        regionwise_best_iou = ious.max(1)  # (|A|,)
+
+        ### c_star encoding
+        c_star = classes[regionwise_best_gt]  # (|A|,)
+        c_star[regionwise_best_iou < self._iou_thresh] = 0
+
+        ### b_star encoding
+        b_star = frcnn_box_encode(
+            regions, boxes[regionwise_best_gt, :]
+        )  # (|A|, 4)
+
+        return c_star, b_star
+
+
+def frcnn_box_encode(anchors: np.ndarray, boxes: np.ndarray) -> np.ndarray:
     """given boxes and anchors, return bounding box offsets from anchors.
     equations for encoding anchor offsets:
         ti = (bi - ai) / ah
@@ -107,9 +154,9 @@ def frcnn_box_encode(boxes: np.ndarray, anchors: np.ndarray) -> np.ndarray:
         tw = log(bw / aw)
 
     Args:
-        boxes: (|A|, 4) anchorwise closest ground truth bounding box.
         anchors: (|A|, 4) bounding box priors. can be deterministically
             precomputed anchors or predicted rois.
+        boxes: (|A|, 4) anchorwise closest ground truth bounding box.
 
     Returns:
         offsets: (|A|, 4) anchor offsets from closest bounding box.
@@ -123,7 +170,7 @@ def frcnn_box_encode(boxes: np.ndarray, anchors: np.ndarray) -> np.ndarray:
     return offsets
 
 
-def frcnn_box_decode(offsets: np.ndarray, anchors: np.ndarray) -> np.ndarray:
+def frcnn_box_decode(anchors: np.ndarray, offsets: np.ndarray) -> np.ndarray:
     """given anchors and anchor offsets, return bounding box coordinates
     equations for decoding anchor offsets:
         bi = ti * ah + ai
@@ -132,9 +179,9 @@ def frcnn_box_decode(offsets: np.ndarray, anchors: np.ndarray) -> np.ndarray:
         tw = exp(tw) * aw
 
     Args:
-        offsets: (|A|, 4) predicted offsets from anchors.
         anchors: (|A|, 4) bounding box priors. can be deterministically
             precomputed anchors or predicted rois.
+        offsets: (|A|, 4) predicted offsets from anchors.
 
     Returns:
         boxes: (|A|, 4) bounding box coordinates.
