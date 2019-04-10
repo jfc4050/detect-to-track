6e2a7b0f3998f3e6d263f918964fd998e1fc7ca2
jfc4050 <jfc4050@gmail.com>
2019-04-09 17:28:05 -0700

    added function for creating anchors

--- detect_to_track/utils.py
+++ detect_to_track/utils.py
@@ -1,6 +1,6 @@
 """misc. utilities"""
 
-from typing import Union, Tuple, Optional
+from typing import Union, Tuple, Optional, Sequence
 
 import numpy as np
 import torch
@@ -22,8 +22,63 @@ def make_input_transform(
 
     return transforms.Compose([
         transforms.Resize(net_input_shape),
-        transforms.ToTensor
+        transforms.ToTensor()
     ])
+
+
+def build_anchors(
+        fm_shape: Union[Tuple[int, int], int],
+        base_area: float,
+        scale_factors: Sequence[float],
+        aspect_ratios: Sequence[float],
+        flatten: bool = True
+) -> np.ndarray:
+    """build flattened anchor grid.
+
+    Args:
+        fm_shape: prediction map height and width.
+        base_area: base anchor area, pre-scaling.
+        scale_factors: anchor area scaling factors.
+        aspect_ratios: anchor aspect ratios (h/w).
+
+    Returns:
+        anchors: (H * W * |SF x AR|, 4) bounding box priors.
+    """
+    if isinstance(fm_shape, int):
+        fm_shape = (fm_shape, fm_shape)
+
+    ### get anchor heights and widths.
+    anchor_dims = np.zeros((len(scale_factors), len(aspect_ratios), 2))
+    for i, scale_factor in enumerate(scale_factors):
+        scaled_area = scale_factor * base_area
+        for j, aspect_ratio in enumerate(aspect_ratios):
+            h = np.sqrt(scaled_area * aspect_ratio)
+            w = scaled_area / h
+            anchor_dims[i, j, :] = [h, w]
+    anchor_dims = anchor_dims.reshape(-1, 2)  # (|SF x AR|, 2)
+
+    ### get centered anchor coordinates.
+    fm_h, fm_w = fm_shape
+    iv, jv = np.meshgrid(
+        np.linspace(0, 1, fm_h + 2)[1:-1],
+        np.linspace(0, 1, fm_w + 2)[1:-1],
+        indexing='ij'
+    )  # 2*(H, W) grid of center i, j values for each feature map cell
+    ij_grid = np.stack([iv, jv], axis=-1)  # (H, W, 2)
+
+    ### expand and concatenate coordinates and shapes.
+    # expand ij_grid and hw_grid: (H, W, |SF x AR|, 2)
+    target_shape = (fm_h, fm_w, len(anchor_dims), 2)
+    ij_grid = np.broadcast_to(ij_grid[:, :, None, :], target_shape)
+    hw_grid = np.broadcast_to(anchor_dims[None, None, :, :], target_shape)
+    anchors = np.concatenate([ij_grid, hw_grid], 3)  # (H, W, |SF x AR|, 4)
+
+    if flatten:
+        anchors = anchors.reshape(-1, 4)  # (H * W * |SF x AR|, 4)
+
+    anchors.flags.writeable = False
+
+    return anchors
 
 
 class DTLoss(object):
