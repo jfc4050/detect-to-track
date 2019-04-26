96619bfa5e263d16f98a43a90f2eb4507e896a41
justin <jfc4050@gmail.com>
2019-04-25 20:04:02 -0700

    added some simple sanity checks

---
+++ checks.py
@@ -0,0 +1,45 @@
+"""simple sanity checks"""
+
+from argparse import ArgumentParser
+from pathlib import Path
+
+
+def check_sampler():
+    from detect_to_track.data.imagenet import VIDTrnSampler, DETSampler
+
+    root = Path('~/datasets/ILSVRC2015').expanduser()
+
+    s = VIDTrnSampler(root)
+    # s = DETSampler(root)
+
+    i0, i1 = s.sample()
+
+    i0.im.show()
+    print(i0.labels)
+    print()
+    print(i1.labels)
+
+
+def check_anchors():
+    from detect_to_track.utils import build_anchors
+
+    anchors = build_anchors((5, 5), [1, 2, 3], [1, 2])
+    print(anchors)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(__doc__)
+    parser.add_argument(
+        '--sampler',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--anchors',
+        action='store_true'
+    )
+    args = parser.parse_args()
+
+    if args.sampler:
+        check_sampler()
+    if args.anchors:
+        check_anchors()
