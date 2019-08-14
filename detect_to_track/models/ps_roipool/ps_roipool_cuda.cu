840ed569e05a72f21f07a6908d76688276e84875
jfc4050 <jfc4050@gmail.com>
2019-08-14 00:19:54 -0700

    fixed issues with PSROIPool
        - handling out of bounds bounding boxes
        - defer clamp op until conversion to ijij coordinates

--- detect_to_track/models/ps_roipool/ps_roipool_cuda.cu
+++ detect_to_track/models/ps_roipool/ps_roipool_cuda.cu
@@ -42,8 +42,8 @@ __global__ void psROIPoolKernelForward(
         // 1. get coordinates of this threads cell c within roi r (ijhw, fractional)
         const scalar_t cH(rH / rHW);  // height of any cell in this roi
         const scalar_t cW(rW / rHW);  // width of any cell in this roi
-        const scalar_t cI(clamp(rI - rH/2) + (static_cast<scalar_t>(i) + 0.5) * cH);  // of cell c
-        const scalar_t cJ(clamp(rJ - rW/2) + (static_cast<scalar_t>(j) + 0.5) * cW);  // of cell c
+        const scalar_t cI(rI - rH/2 + (static_cast<scalar_t>(i) + 0.5) * cH);  // of cell c
+        const scalar_t cJ(rJ - rW/2 + (static_cast<scalar_t>(j) + 0.5) * cW);  // of cell c
         // 2. convert from ijhw to ijij and clamp to [0, 1].
         // 3. convert from fractional to pixel coordinates.
         // 4. take floor of i0, j0 and ceil of i1, j1.
@@ -64,7 +64,9 @@ __global__ void psROIPoolKernelForward(
             }
         }
         // average pool - divide by pooledNumel
-        out[ind] /= ((cI1 - cI0) * (cJ1 - cJ0));
+        const int pooledNumel((cI1 - cI0) * (cJ1 - cJ0));
+        if (pooledNumel > 0)
+            out[ind] /= pooledNumel;
     }
 }
 
@@ -107,8 +109,8 @@ __global__ void psROIPoolKernelBackward(
         // 1. get coordinates of this threads cell c within roi r (ijhw, fractional)
         const scalar_t cH(rH / rHW);  // height of any cell in this roi
         const scalar_t cW(rW / rHW);  // width of any cell in this roi
-        const scalar_t cI(clamp(rI - rH/2) + (static_cast<scalar_t>(i) + 0.5) * cH);  // of cell c
-        const scalar_t cJ(clamp(rJ - rW/2) + (static_cast<scalar_t>(j) + 0.5) * cW);  // of cell c
+        const scalar_t cI(rI - rH/2 + (static_cast<scalar_t>(i) + 0.5) * cH);  // of cell c
+        const scalar_t cJ(rJ - rW/2 + (static_cast<scalar_t>(j) + 0.5) * cW);  // of cell c
         // 2. convert from ijhw to ijij and clamp to [0, 1].
         // 3. convert from fractional to pixel coordinates.
         // 4. take floor of i0, j0 and ceil of i1, j1.
@@ -129,7 +131,10 @@ __global__ void psROIPoolKernelBackward(
         for (int pI = cI0; pI < cI1; ++pI) {
             for (int pJ = cJ0; pJ < cJ1; ++pJ) {
                 // dL/dIn = dL/dOut * (dOut/dIn = 1/n)
-                atomicAdd(gradInChannel + (pI * iW + pJ), gradOut[ind] / roiNumel);
+                scalar_t addVal(gradOut[ind]);
+                if (roiNumel > 0)
+                    addVal /= roiNumel;
+                atomicAdd(gradInChannel + (pI * iW + pJ), addVal);
             }
         }
     }
--- tests/test_ps_roipool.py
+++ tests/test_ps_roipool.py
@@ -19,7 +19,7 @@ def test_ps_roipool_gradients(n_targets, r_hw, fm_h, fm_w):
     )
 
     rois = (
-        torch.Tensor([[0.5, 0.5, 0.1, 0.1], [0.1, 0.1, 0.2, 0.3]])
+        torch.Tensor([[0.5, 0.5, 0.1, 0.1], [0.1, 0.1, 0.2, 0.3], [1.5, 1.5, 0.2, 0.2]])
         .double()
         .cuda()
         .requires_grad_(False)
@@ -28,3 +28,17 @@ def test_ps_roipool_gradients(n_targets, r_hw, fm_h, fm_w):
     grad_pass = gradcheck(pr, (fm, rois))
 
     assert grad_pass
+
+
+@pytest.mark.parametrize("n_targets", [1, 2])
+@pytest.mark.parametrize("r_hw", [6, 7])
+@pytest.mark.parametrize("fm_h", [10, 11])
+@pytest.mark.parametrize("fm_w", [10, 11])
+def test_ps_roipool_can_handle_oob(n_targets, r_hw, fm_h, fm_w):
+    pr = PSROIPool(n_targets, r_hw).cuda()
+    fm = torch.full((n_targets * r_hw ** 2, fm_h, fm_w), 10).cuda()
+    rois = torch.as_tensor([[3.0, 3.0, 0.5, 0.5]]).cuda()
+
+    ans = pr(fm, rois).cpu()
+
+    assert torch.allclose(ans, torch.zeros(len(rois), n_targets, r_hw, r_hw))
